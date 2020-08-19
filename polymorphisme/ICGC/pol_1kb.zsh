13f8912d47851a9f7709509403c4a542da141eb2
marie-michel <40595698+marie-michel@users.noreply.github.com>
2020-08-19 18:34:15 +0200

    Update pol_1kb.zsh

--- polymorphisme/ICGC/pol_1kb.zsh
+++ polymorphisme/ICGC/pol_1kb.zsh
@@ -15,11 +15,9 @@
 tabix pcawg8.snps.indels.svs.phased.icgc.v2.controlled.vcf.gz
 tabix -h pcawg8.snps.indels.svs.phased.icgc.v2.controlled.vcf.gz 9:21016240-21018240 > pol_1kb.vcf
 
-while IFS= read -r line; do
-		if ! [[ $line =~ (^#) ]]; then awk -F"\t" 'BEGIN {print "CHR\tPOS\tID\tREF\tALT\tnTot\tnHet\tnHomRef\tnHomAlt\tnref\tnalt"} \
-	      {nhet=gsub(/0\|1|1\|0|0\/1|1\/0/, ""); nhomref=gsub(/0\|0|0\/0/, ""); nhomalt=gsub(/1\|1|1\/1/, ""); ntot=(nhet+nhomref+nhomalt);
-	        print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"ntot"\t"nhet"("(nhet/ntot)*100"%)\t"nhomref"("(nhomref/ntot)*100"%)\t"nhomalt"("(nhomalt/ntot)*100"%)\t"(nhomref*2+nhet)"("((nhomref*2+nhet)/(ntot*2))*100"%)\t"(nhomalt*2+nhet)"("((nhomalt*2+nhet)/(ntot*2))*100"%)"}' > pol_stat_1kb.vcf; fi
-done < pol_1kb.vcf
+#while IFS= read -r line; do
+#		if ! [[ $line =~ (^#) ]]; then awk -F"\t" 'BEGIN {print "CHR\tPOS\tID\tREF\tALT\tnTot\tnHet\tnHomRef\tnHomAlt\tnref\tnalt"} \
+#	      {nhet=gsub(/0\|1|1\|0|0\/1|1\/0/, ""); nhomref=gsub(/0\|0|0\/0/, ""); nhomalt=gsub(/1\|1|1\/1/, ""); ntot=(nhet+nhomref+nhomalt);
+#	        print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"ntot"\t"nhet"("(nhet/ntot)*100"%)\t"nhomref"("(nhomref/ntot)*100"%)\t"nhomalt"("(nhomalt/ntot)*100"%)\t"(nhomref*2+nhet)"("((nhomref*2+nhet)/(ntot*2))*100"%)\t"(nhomalt*2+nhet)"("((nhomalt*2+nhet)/(ntot*2))*100"%)"}' > pol_stat_1kb.vcf; fi
+#done < pol_1kb.vcf
 
-#snps randoms : 
-tabix -h pcawg8.snps.indels.svs.phased.icgc.v2.controlled.vcf.gz 9 | shuf -n 10  > random.vcf
